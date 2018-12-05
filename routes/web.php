9ae00cd6197834402cbfc5c0a7abc719efcbfd7e
yangbin <yss920@163.com>
2018-12-05 03:59:03 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,8 @@ Route::post('/users', 'UsersController@store')->name('users.store');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.eidt');
 Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destory')->name('logout');

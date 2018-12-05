bcaea275f69942a2f488ea0523c9aff0f5e46488
yangbin <yss920@163.com>
2018-12-05 12:12:21 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = 'yss920@163.com';
         $user->password = bcrypt('123456');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy')
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destory')->name('logout');
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
